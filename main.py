724e204bdbacffe1fba94c36380b2c3ea5cae4b5
LUCIANOGFORTES02 <lucianogfortes@gmail.com>
2022-12-29 12:05:27 -0300

    corrigido

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import requests #Biblioteca que permite fazer requisições 
+import requests 
+import telebot 
 import time
 import json
 
@@ -6,11 +7,11 @@ class Bot:
     def __init__ (self):
         token='5810812199:AAFoebmRT8YjizOCA1DdKVScn4I7cRpoyoM'
         self.url_base = f'https://api.telegram.org/bot{token}/'
-
+# Quando se coloca uma / e uma palavra se torna um link
 
 #iniciar bot
     def iniciar(self): 
-        update_id= None
+        update_id = None
         while True:
             atualizacao = self.obterMensagens(update_id)
             mensagens = atualizacao['result']
@@ -19,30 +20,27 @@ class Bot:
                         update_id = mensagem ['update_id'] 
                         chat_id = mensagem ['message']['from']['id']#Para quem vai enviar a mensagem
                         resposta = self.criarResposta()
+                        print(resposta)
                         self.responder(resposta,chat_id)
-
-            
-
-
 
 #Obter mensagem 
     def obterMensagens (self,update_id):
         link_requisicao = f'{self.url_base}getUpdates?timeout=100'
-
         if update_id:
             #Adiciona um parâmetro que vai permitir pegar apenas a última mesagem
-            link_requisicao= f'{link_requisicao}&offset={update_id+1}'
+            link_requisicao = f'{link_requisicao}&offset={update_id+1}'
         resultado =requests.get(link_requisicao)
+        print(json.loads(resultado.content))
         return json.loads(resultado.content)
 
 #Criar uma resposta 
     def criarResposta (self):
-        return 'oá'
+        return "olá"
 #Responder
     def responder(self,resposta,chat_id):
         #enviar
         #link formatado com parametro sendMessage passando para qual chat sera enviado e a resposta enviada
-        link_de_envio = f'{self.url_base}sendMensage?chat_id={chat_id}&text={resposta}'
+        link_de_envio = f'{self.url_base}sendMessage?chat_id={chat_id}&text={resposta}'
         requests.get(link_de_envio) 
 
 
